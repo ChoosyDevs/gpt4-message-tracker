3a460c86b21de0c552a0580abbbd134aa1a0d5f8
Nikos Vlachakis <nvlachakis1999@gmail.com>
2023-12-10 02:33:56 +0200

    Fix request count reset and update badge text

--- background.js
+++ background.js
@@ -1,19 +1,27 @@
 // Initialize global variables
-let requestCount = 0;
-let first_message_timestamp = Date.now() - 24 * 60 * 60 * 1000; 
 let windowDuration = 60 * 1000; // 1 min 
+let max_num_requests = 40;
 
 // Function to reset the request count if the current time exceeds the window duration
 function checkAndReset() {
     // console.log in seconds till reset
-    console.log("Seconds till reset:", (windowDuration - (Date.now() - first_message_timestamp)) / 1000);
+    console.log("Seconds till reset:", (windowDuration - (Date.now() - localStorage.getItem('first_message_timestamp'))) / 1000);
    
-    if (Date.now() - first_message_timestamp > windowDuration) {
-        requestCount = 0;
-        chrome.browserAction.setBadgeText({text: requestCount.toString()});
-        first_message_timestamp = Date.now();
+    if (Date.now() - localStorage.getItem('first_message_timestamp') > windowDuration) {
+        localStorage.setItem('request_count', 0);
+        localStorage.setItem('first_message_timestamp', Date.now());
+
         console.log("Count reset. Next reset scheduled in 3 hours.");
     }
+    let requestCount = parseInt(localStorage.getItem('request_count'));
+    // check if request count is null
+    if (!requestCount) {
+        requestCount = 0
+        localStorage.setItem('request_count', 0);
+    }
+       
+    chrome.browserAction.setBadgeText({text: requestCount.toString() + "/" + max_num_requests.toString()});
+
 }
 
 // Function to track GPT-4 requests and increment the request count
@@ -21,9 +29,15 @@ function trackGPT4Request(details) {
     console.log("Request detected:", details.url);
     if (details.url.includes('https://chat.openai.com/backend-api/conversation')) {
         checkAndReset();
+        let requestCount = parseInt(localStorage.getItem('request_count'));
+        if (!requestCount) {
+            requestCount = 0
+        }
         requestCount++;
+        localStorage.setItem('request_count', requestCount);
+
         console.log("GPT-4 request count incremented:", requestCount);
-        chrome.browserAction.setBadgeText({text: requestCount.toString()});
+        chrome.browserAction.setBadgeText({text: requestCount.toString() + "/" + max_num_requests.toString()});
     }
 }
 
@@ -33,23 +47,6 @@ chrome.webRequest.onBeforeRequest.addListener(
     {urls: ["https://chat.openai.com/backend-api/conversation"]},
     ["requestBody"]
 );
-
-// Event listener for extension startup, calls checkAndReset function
-chrome.runtime.onStartup.addListener(function() {
-    checkAndReset();
-});
-
-// Event listener for extension installation, calls checkAndReset function
-chrome.runtime.onInstalled.addListener(function() {
-    checkAndReset();
-});
-
-// Event listener for tab updates (reload), calls checkAndReset function
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-        checkAndReset();
-    }
-});
 
 // Set an interval to call checkAndReset function every SECOND
 setInterval(checkAndReset, 1 * 1000);
